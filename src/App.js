c2550f37007d129ae2bfef36379d79623c7f2e50
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-03-15 09:53:12 +0530

    chore: added mockman

--- src/App.js
+++ src/App.js
@@ -1,10 +1,20 @@
 import "./App.css";
-import {Nav} from "./Pages/Nav"
-import {LandingPage} from "./Pages/LandingPage"
+import { Routes, Route } from "react-router-dom";
+import { Link } from "react-router-dom";
+
+import MockAPI from "./mockman";
+import {Nav} from "./Pages/Nav";
+import {LandingPage} from "./Pages/LandingPage";
 
 function App() {
+
+
+
   return (
     <div>
+      <Routes>
+        <Route path="/mock-api" element={<MockAPI />} />
+      </Routes>
       <Nav/>
       <LandingPage/>
     </div>
