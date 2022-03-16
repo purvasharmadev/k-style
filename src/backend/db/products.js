5431fa75319a16c936db53e600433dcb07121f93
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-03-16 12:02:07 +0530

    chore: added product data

--- src/backend/db/products.js
+++ src/backend/db/products.js
@@ -11,14 +11,53 @@ export const products = [
     title: "You Can WIN",
     author: "Shiv Khera",
     price: "5000",
+    featured:true,
     categoryName: "non-fiction",
     img:"https://cdn.shopify.com/s/files/1/0564/4320/7839/products/product-image-1701761661_large_crop_center.jpg?v=1620119459"
   },
   {
     _id: uuid(),
+    title: "You Can WIN",
+    author: "Shiv Khera",
+    price: "5000",
+    featured:true,
+    categoryName: "non-fiction",
+    img:"https://cdn.shopify.com/s/files/1/0564/4320/7839/products/product-image-1701761661_large_crop_center.jpg?v=1620119459"
+  },
+  {
+    _id: uuid(),
+    title: "You Can WIN",
+    author: "Shiv Khera",
+    price: "5000",
+    featured:true,
+    categoryName: "non-fiction",
+    img:"https://cdn.shopify.com/s/files/1/0564/4320/7839/products/product-image-1701761661_large_crop_center.jpg?v=1620119459"
+  },
+  {
+    _id: uuid(),
+    title: "You Can WIN",
+    author: "Shiv Khera",
+    price: "5000",
+    featured:true,
+    categoryName: "non-fiction",
+    img:"https://cdn.shopify.com/s/files/1/0564/4320/7839/products/product-image-1701761661_large_crop_center.jpg?v=1620119459"
+  },
+  {
+    _id: uuid(),
+    title: "You Can WIN",
+    author: "Shiv Khera",
+    price: "5000",
+    featured:true,
+    categoryName: "non-fiction",
+    img:"https://cdn.shopify.com/s/files/1/0564/4320/7839/products/product-image-1701761661_large_crop_center.jpg?v=1620119459"
+  },
+
+  {
+    _id: uuid(),
     title: "You are Winner",
     author: "Junaid Qureshi",
     price: "3000",
+    featured:true,
     categoryName: "horror",
     img:"https://cdn.shopify.com/s/files/1/0564/4320/7839/products/product-image-1701761661_large_crop_center.jpg?v=1620119459"
   },
@@ -27,6 +66,34 @@ export const products = [
     title: "Think and Grow Rich",
     author: "Shiv Khera",
     price: "1000",
+    featured:false,
+    categoryName: "fiction",
+    img:"https://cdn.shopify.com/s/files/1/0564/4320/7839/products/product-image-1701761661_large_crop_center.jpg?v=1620119459"
+  },
+  {
+    _id: uuid(),
+    title: "Think and Grow Rich",
+    author: "Shiv Khera",
+    price: "1000",
+    featured:false,
+    categoryName: "fiction",
+    img:"https://cdn.shopify.com/s/files/1/0564/4320/7839/products/product-image-1701761661_large_crop_center.jpg?v=1620119459"
+  },
+  {
+    _id: uuid(),
+    title: "Think and Grow Rich",
+    author: "Shiv Khera",
+    price: "1000",
+    featured:true,
+    categoryName: "fiction",
+    img:"https://cdn.shopify.com/s/files/1/0564/4320/7839/products/product-image-1701761661_large_crop_center.jpg?v=1620119459"
+  },
+  {
+    _id: uuid(),
+    title: "Think and Grow Rich",
+    author: "Shiv Khera",
+    price: "1000",
+    featured:true,
     categoryName: "fiction",
     img:"https://cdn.shopify.com/s/files/1/0564/4320/7839/products/product-image-1701761661_large_crop_center.jpg?v=1620119459"
   },
