60987209cd7b6223ebace50c747dd1381c2a38b8
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-03-17 19:08:40 +0530

    delete: deleted description

--- src/backend/db/categories.js
+++ src/backend/db/categories.js
@@ -9,37 +9,37 @@ export const categories = [
   {
     _id: uuid(),
     categoryName: "T-Shirt",
-    categoryImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiSnYuHOvLxpUtV9_mbOA-sIBHhX-K9kHemyUPcYSbh8wXDRf2IOxElezvutlx6hyKQo0&usqp=CAU",
+    categoryImg:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiSnYuHOvLxpUtV9_mbOA-sIBHhX-K9kHemyUPcYSbh8wXDRf2IOxElezvutlx6hyKQo0&usqp=CAU",
   },
   {
     _id: uuid(),
     categoryName: "Hoodies",
-    categoryImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiSnYuHOvLxpUtV9_mbOA-sIBHhX-K9kHemyUPcYSbh8wXDRf2IOxElezvutlx6hyKQo0&usqp=CAU",
+    categoryImg:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiSnYuHOvLxpUtV9_mbOA-sIBHhX-K9kHemyUPcYSbh8wXDRf2IOxElezvutlx6hyKQo0&usqp=CAU",
   },
   {
     _id: uuid(),
     categoryName: "Idol Fashion",
-    categoryImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiSnYuHOvLxpUtV9_mbOA-sIBHhX-K9kHemyUPcYSbh8wXDRf2IOxElezvutlx6hyKQo0&usqp=CAU",
+    categoryImg:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiSnYuHOvLxpUtV9_mbOA-sIBHhX-K9kHemyUPcYSbh8wXDRf2IOxElezvutlx6hyKQo0&usqp=CAU",
   },
   {
     _id: uuid(),
     categoryName: "Albums",
-    categoryImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiSnYuHOvLxpUtV9_mbOA-sIBHhX-K9kHemyUPcYSbh8wXDRf2IOxElezvutlx6hyKQo0&usqp=CAU",
-    description:
-      "Non-fiction is writing that gives information or describes real events, rather than telling a story.",
+    categoryImg:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiSnYuHOvLxpUtV9_mbOA-sIBHhX-K9kHemyUPcYSbh8wXDRf2IOxElezvutlx6hyKQo0&usqp=CAU",
   },
   {
     _id: uuid(),
     categoryName: "LightSticks",
-    categoryImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiSnYuHOvLxpUtV9_mbOA-sIBHhX-K9kHemyUPcYSbh8wXDRf2IOxElezvutlx6hyKQo0&usqp=CAU",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    categoryImg:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiSnYuHOvLxpUtV9_mbOA-sIBHhX-K9kHemyUPcYSbh8wXDRf2IOxElezvutlx6hyKQo0&usqp=CAU",
   },
   {
     _id: uuid(),
     categoryName: "POCA",
-    categoryImg:"https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiSnYuHOvLxpUtV9_mbOA-sIBHhX-K9kHemyUPcYSbh8wXDRf2IOxElezvutlx6hyKQo0&usqp=CAU",
-    description:
-      "Meant to cause discomfort and fear for both the character and readers, horror writers often make use of supernatural and paranormal elements in morbid stories that are sometimes a little too realistic.",
+    categoryImg:
+      "https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQiSnYuHOvLxpUtV9_mbOA-sIBHhX-K9kHemyUPcYSbh8wXDRf2IOxElezvutlx6hyKQo0&usqp=CAU",
   },
 ];
