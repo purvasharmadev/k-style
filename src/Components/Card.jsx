a2ecf823b464ad204b7c7927ef4c20f481de5a2f
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-03-19 13:45:14 +0530

    docs: renamed 'Product' to 'Card' component

---
+++ src/Components/Card.jsx
@@ -0,0 +1,29 @@
+import React from 'react'
+
+
+function ProductCard (props) {
+    
+    return ( 
+        <div key={props.id} class="card ecom-card">
+        <div class="card-header position-relative">
+            <span class="card-badge"> ❤ </span>
+            <span class="ecom-badge"> New Arrival </span>
+             <img height="268px" width="268px" loading="lazy"
+                src={props.img} alt="productImg" />
+            <span class="category">{props.categoryName}</span>
+        </div>
+        <div class="card-body">
+            <h3 class="card-heading">{props.title}</h3>
+            <p class="card-text">
+              $  {props.price} <span class="ecom-card-price">{props.oldPrice}</span> 
+            </p>
+        </div>
+        <div class="card-footer">
+            <a href="/cart-management/cart.html" class="btn btn-primary">Add To Cart</a>
+        </div>
+    </div>
+
+     );
+}
+
+export {ProductCard} ;
