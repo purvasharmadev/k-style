e7e127dc63928394958dc4dc0cbd01d34767460e
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-03-26 10:28:28 +0530

    feat: added context for products and filter

---
+++ src/Context/context.js
@@ -0,0 +1,35 @@
+import { createContext, useContext, useReducer } from "react";
+import { useGetProducts } from "../Hooks/useGetProducts";
+import {filterReducer} from "../Reducers/filterReducer"
+
+
+const ProductContext = createContext();
+
+const ProductProvider = ({ children }) => {
+
+  // getting products , error , loader by hook
+  const { Products, errorMsg, loader } = useGetProducts();
+
+  //  Filter Reducer
+  const [filterState, filterDispatch] = useReducer(filterReducer, {
+    sort:'',
+    rating:'',
+    search_query:'',
+    POCA:false,
+    Album:false,
+    LightSticks:false,
+    IdolFashion:false,
+    TSHIRT: false,
+    HOODIES: false,
+  });
+
+  console.log("filterState: ", filterState)
+  return (
+    <ProductContext.Provider value={{ Products, loader, errorMsg, filterDispatch, filterState }}>
+      {children}
+    </ProductContext.Provider>
+  );
+};
+
+const useProducts = () => useContext(ProductContext);
+export { ProductProvider, useProducts };
