b9c867dfac0ff90f2546e3ff6d1306294d7b6653
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-03-29 13:43:33 +0530

    feat: added signup feature

--- src/Components/auth/signup.jsx
+++ src/Components/auth/signup.jsx
@@ -1,48 +1,101 @@
 import { Link } from "react-router-dom";
 import { Footer } from "../../Pages/Footer";
 import { Nav } from "../../Pages/Nav";
+import { useSignUp } from "../../Hooks/useSignupUser";
 
 function SignUp() {
+  const { sucess, error, setUserData, userData, postUserSignIn } = useSignUp();
+  function signUpHandler(event) {
+    console.log("signUphandler running........");
+    event.preventDefault();
+    postUserSignIn();
+  }
+
   return (
     <>
       <Nav />
-      <div className="input-container mt-top">
-        <h2 class="form-heading">Signup</h2>
+      {sucess ? (
+        <div className="input-container">
+          <h2 className="form-heading p-1">
+            You have been sucessfully register in!
+          </h2>
+          <h3 className="color-warning p-1">
+            Now you can login and add/delete items to cart and wishlist!
+          </h3>
+          <Link to="/login" className="link w-50 p-1">
+            Login Here !!
+          </Link>
+        </div>
+      ) : (
+        <div className="input-container mt-top">
+          <h2 className="form-heading">Signup</h2>
 
-        <form action="" className="form-container">
-          <label for="username">User Name</label>
-          <input type="text" name="username" placeholder="enter your name" />
-          <label for="email">Email</label>
-          <input type="email" name="email" placeholder="yourname@mail.com" />
-          <label for="password">Password </label>
-          <input type="password" name="password" placeholder="********" />
-          <label for="confirm-password">Confirm Password </label>
-          <input
-            type="password"
-            name="confirm-password"
-            placeholder="********"
-          />
-          <label for="terms-and-agrement">
+          <form onSubmit={signUpHandler} className="form-container">
+            <label htmlFor="first-name">first Name</label>
             <input
-              type="checkbox"
-              name="remember-me"
-              id="remember-me-check "
-              checked={true}
+              type="text"
+              name="first-name"
+              value={userData.firstName}
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, firstName: e.target.value }))
+              }
+              placeholder="first name"
             />
-            I accept all terms and conditions
-          </label>
+            <label htmlFor="last-name">Last Name</label>
+            <input
+              type="text"
+              name="last-name"
+              value={userData.lastName}
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, lastName: e.target.value }))
+              }
+              placeholder="last name"
+            />
+            <label htmlFor="email">Email</label>
+            <input
+              className={error ? "error" : ""}
+              type="email"
+              name="email"
+              value={userData.email}
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, email: e.target.value }))
+              }
+              placeholder="yourname@mail.com"
+            />
+            {error && <p className="error-msg p-1"> * Email already exists</p>}
+            <label htmlFor="password">Password </label>
+            <input
+              type="password"
+              name="password"
+              value={userData.password}
+              onChange={(e) =>
+                setUserData((prev) => ({ ...prev, password: e.target.value }))
+              }
+              placeholder="********"
+            />
+            <label htmlFor="terms-and-agrement">
+              <input
+                type="checkbox"
+                name="terms-and-condition"
+                id="T-n-C-check "
+                checked={true}
+              />
+              I accept all terms and conditions
+            </label>
 
-          <a class="btn btn-primary " href="/auth/sign-up.html">
-            Create New Account
-          </a>
+            <button type="submit" className="btn btn-primary ">
+              Create New Account
+            </button>
 
-          <h4>
-            <Link to="/login" class="link">
-              Already have a Account <i class="fa fa-arrow-right fa-x"></i>{" "}
-            </Link>
-          </h4>
-        </form>
-      </div>
+            <h4>
+              <Link to="/login" className="link">
+                Already have a Account{" "}
+                <i className="fa fa-arrow-right fa-x"></i>{" "}
+              </Link>
+            </h4>
+          </form>
+        </div>
+      )}
       <Footer />
     </>
   );
