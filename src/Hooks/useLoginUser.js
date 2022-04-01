215ef23f1e34864bc79a799e2a6583dcc3275abd
Purva Sharma <71826879+purvasharmadev@users.noreply.github.com>
2022-04-01 17:03:53 +0530

    docs: getting token in a variable

--- src/Hooks/useLoginUser.js
+++ src/Hooks/useLoginUser.js
@@ -2,15 +2,12 @@ import { useState, useEffect } from "react";
 import axios from "axios";
 
 function useLogin() {
-
   const [response, setResponse] = useState(undefined);
 
-  const [userData,setUserData] = useState({
-    email:"",password:""
-  })
-
-  const [ userName, setUserName] = useState('User')
-
+  const [userData, setUserData] = useState({
+    email: "",
+    password: "",
+  });
 
   // useState for sucess
   const [sucess, setSucess] = useState(false);
@@ -18,39 +15,29 @@ function useLogin() {
   //   useState for error
   const [error, setError] = useState(false);
 
-
   // Api call
   async function postUserLogin() {
     try {
-   const res =  await axios.post("/api/auth/login",userData);
-   res.headers["*/*"]
-   setResponse(res.data)
-
-        
+      const res = await axios.post("/api/auth/login", userData);
+      res.headers["*/*"];
+      setResponse(res.data);
     } catch (error) {
-      console.log("error is: ", error)
+      console.log("error is: ", error);
       setError(true);
     }
   }
 
   useEffect(() => {
-    if(response !== undefined){
-          localStorage.setItem("token", response.encodedToken)
-          setUserName(response.foundUser.firstName)
-          console.log(userName)
-
-          setSucess(true);
-          setError(false);
+    if (response !== undefined) {
+      localStorage.setItem("token", response.encodedToken);
+      setSucess(true);
+      setError(false);
     }
   }, [response]);
 
+  const token = localStorage.getItem("token");
 
-
-
-  return { error, sucess, postUserLogin, setUserData,userData};
+  return { error, sucess, postUserLogin, setUserData, userData, token };
 }
-
-
-
 
 export { useLogin };
